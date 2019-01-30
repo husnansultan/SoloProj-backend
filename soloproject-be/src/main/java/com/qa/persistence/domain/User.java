ffb27dd2212cf42d6e733713bb3ddf79296f8788
husnansultan <husnan.sultan@academytrainee.com>
2019-01-30 09:23:14 +0000

    stil not working
    no workigh

--- soloproject-be/src/main/java/com/qa/persistence/domain/User.java
+++ soloproject-be/src/main/java/com/qa/persistence/domain/User.java
@@ -1,6 +1,5 @@
 package com.qa.persistence.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -8,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 @Entity
@@ -23,7 +21,6 @@ public class User {
 	private String password;
 
 	@OneToMany(mappedBy = "userid", cascade = CascadeType.ALL)
-	@JoinColumn(name = "userid")
 	private List<Order> orders;
 
 	public User() {
--- soloproject-be/src/main/resources/import.sql
+++ soloproject-be/src/main/resources/import.sql
@@ -1,2 +1,8 @@
-INSERT INTO User (name, email, password) values (1, 'jim', 'jim', 'jim@jim')
+INSERT INTO User (name, email, password) values ('jim', 'jim@jim.com', 'jim')
 INSERT INTO Order (orderid, userid) values (1, 1)
+
+INSERT INTO Food (name, category, price) values ('chicken pakora', 'starters', 5)
+INSERT INTO Food (name, category, price) values ('vegetab pakora', 'starters', 5)
+INSERT INTO Food (name, category, price) values ('ch pakora', 'starters', 5)
+INSERT INTO Food (name, category, price) values ('cken pakora', 'starters', 5)
+INSERT INTO Food (name, category, price) values ('n pakora', 'starters', 5)
